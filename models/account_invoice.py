f163ec7e197d47b85ac71859b758010aa378c2b0
Tony Galmiche <tony.galmiche@infosaone.com>
2022-12-28 09:12:03 +0100

    modifs

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -34,17 +34,22 @@ class AccountInvoice(models.Model):
     def _compute_montant_revision(self):
         for obj in self:
             montant = 0
-            montant_hors_revision = 0
             for line in obj.invoice_line_ids:
                 if line.product_id.name==u'Révisions':
                     montant += line.price_unit*line.quantity
             obj.is_montant_revision = montant
-            obj.is_montant_hors_revision = obj.amount_untaxed - montant
+            montant_hors_revision = obj.amount_untaxed - montant
+            obj.is_montant_hors_revision = montant_hors_revision
+            if obj.type=="out_refund":
+                obj.is_montant_hors_revision_signe = - montant_hors_revision
+            else:
+                obj.is_montant_hors_revision_signe = montant_hors_revision
 
     is_contrat_id            = fields.Many2one('is.dossier.contrat', u'Contrat', index=True)
     is_total_contrat_ht      = fields.Float(u"Total contrat HT"     , digits=(14,2), compute='_compute_total_contrat_ht', readonly=True, store=False)
     is_montant_revision      = fields.Float(u"Montant révision"     , digits=(14,4), compute='_compute_montant_revision', readonly=True, store=True)
     is_montant_hors_revision = fields.Float(u"Montant hors révision", digits=(14,4), compute='_compute_montant_revision', readonly=True, store=True)
+    is_montant_hors_revision_signe = fields.Float(u"Montant hors révision signé", digits=(14,4), compute='_compute_montant_revision', readonly=True, store=True)
     is_dossier_id            = fields.Many2one('is.dossier', u"Dossier"       , compute='_compute_dossier_id'      , readonly=True, store=True, index=True)
     is_tva_id                = fields.Many2one('is.tva', u'TVA')
     is_date_update_facture   = fields.Datetime("Date mise à jour déjà facturé")
